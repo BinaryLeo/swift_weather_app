3259404edf8ea072922461eabd23c157b0e1b797
Binaryleo <leomouracss@fastmail.com>
2022-07-09 02:47:09 -0300

    Initial Commit

---
+++ swift_weather_app/swift_weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_weather_appApp.swift
+//  swift_weather_app
+//
+//  Created by Leo moura on 09/07/22.
+//
+
+import SwiftUI
+
+@main
+struct swift_weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
